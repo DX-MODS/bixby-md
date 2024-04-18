3808003522220dfa39f93baab2fc8aad4fe4a2ae
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-04-19 00:06:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const obfuscateCode = require('./lib/obfuscator');
-const beautifyCode = require('./lib/js-pretifier');
+const beautifyjs = require('./lib/beautify-js');
 module.exports = {
     obfuscateCode,
-    beautifyCode
+    beautifyjs,
 };
